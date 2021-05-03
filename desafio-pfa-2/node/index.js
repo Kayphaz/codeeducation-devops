65cd61e2390eda6326ba95f9c96d78c991a55fad
Kayphaz <caifasjr@gmail.com>
2021-05-03 13:37:30 -0300

    chore: add desafio pfa 2

---
+++ desafio-pfa-2/node/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const knex = require('knex');
+const cors = require('cors');
+
+const db = knex({
+    client: 'mysql',
+    connection: {
+        host: 'db',
+        user: 'root',
+        password: 'root',
+        database: 'nodedb'
+    }
+})
+const app = express();
+
+const PORT = 3000;
+app.use(cors());
+app.use(express.static('./html'));
+
+db.schema.createTableIfNotExists('modulos', table => {
+    table.increments();
+    table.string('name');
+}).then('Created');
+// Insert name on server start
+db.insert({name: 'Docker'}).into('modulos').then('Inserted');
+db.insert({name: 'Kubernetes'}).into('modulos').then('Inserted');
+db.insert({name: 'Observabilidade'}).into('modulos').then('Inserted');
+
+app.get('/list', (req, res) => db.select('*').table('modulos').then(names => res.json(names)));
+
+app.listen(PORT, () => console.log(`Server running at localhost:${PORT}`));
